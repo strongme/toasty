9ed6b2496a6d26c9b2afe11a639085d7a535c9cc
阿水 <strongwalter2014@gmail.com>
2020-03-15 20:53:27 +0800

    fix

--- www/ToastyPlugin.js
+++ www/ToastyPlugin.js
@@ -3,7 +3,7 @@ var exec = require('cordova/exec');
 const ToastyPlugin = function() {
     //
 }
-Echo.prototype.show = function (arg0, success, error) {
+ToastyPlugin.prototype.show = function (arg0, success, error) {
     exec(success, error, 'ToastyPlugin', 'show', [arg0]);
 };
 
